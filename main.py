9037d2dea1f4887f67453c52c1bfe1660a5d3cf4
Y V Suyash Murthy <suyash.murthy@gmail.com>
2025-10-23 13:19:15 +0530

    route changes testing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return JSONResponse({"message": "FastAPI running on Vercel!"})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
